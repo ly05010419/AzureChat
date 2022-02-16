6466152b49d659e58ff19a3ddc08ed80539d3dc8
Yong Li <Yong.Li@msg.group>
2022-02-16 12:02:25 +0100

    update

--- ChatApp/ChatApp.Android/MainActivity.cs
+++ ChatApp/ChatApp.Android/MainActivity.cs
@@ -4,6 +4,9 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using ChatApp.Mobile;
+using Prism;
+using Prism.Ioc;
 
 namespace ChatApp.Droid
 {
@@ -16,13 +19,22 @@ namespace ChatApp.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
+            LoadApplication(new App(new AndroidInitializer()));
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        
+    }
+    
+    public class AndroidInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // Register any platform specific implementations
         }
     }
 }
